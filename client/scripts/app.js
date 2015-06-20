48305720419b4ae27963f444d5306bf0aa7f5d0e
MKS-Student <mks.student@hackreactor.com>
2015-06-20 11:09:04 -0700

    Working chat functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -10,10 +10,26 @@ var app = {
   message: {}, 
 
   init: function() {
-    app.fetch(); // retrieves data object filled of messages
-    $('#send').on('click', function() { app.handleSubmit($("#chatMessage").val()) });
-    app.message.username 
-    $('#user').on('click', function() { app.addFriend() } );
+    // retrieves data object filled of messages
+    app.fetch(); 
+    // grab username from URL
+    var myUsername = location.search.split('username=')[1];
+    // creates message object and send to handle submit method
+    var room;
+    $('#send').on('click', function() { app.message.text =  $("#chatMessage").val()
+                                        app.message.username = myUsername;
+                                          if (room) {
+                                            app.message.roomname = room;
+                                          } else {
+                                            // defaults to lobby if no room name given
+                                            app.message.roomname = 'lobby'
+                                          }
+                                        app.handleSubmit(app.message);
+                                        })
+    // add username to friends list
+    $('#user').on('click', function() { app.addFriend($("#user").val() ) } );
+    // add room name to #roomSelect div
+    $('#addRoom').on('click', function() { app.addRoom(roomName) })
   },
 
   send: function(message) {
@@ -24,7 +40,7 @@ var app = {
       data: JSON.stringify(message),
       contentType: 'application/json',
       success: function (data) {
-        console.log('chatterbox: Message sent');
+        console.log('chatterbox: Message sent: "' + message.text + '" by ' + message.username + " in room " + message.roomname + ".");
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -32,12 +48,6 @@ var app = {
       }
     })
   },
-
-  // displayMessages: function(messages) {
-  //   for (var key in messages) {
-  //     app.addMessages(messages[key].text);
-  //   }
-  // },
 
   fetch: function(message) {
     $.ajax({
@@ -48,9 +58,11 @@ var app = {
       contentType: 'application/json',
       success: function (data) {
         // console.log('chatterbox: Message sent');
+        console.log(data)
+
         app.container = data;
         for (var key in app.container.results) {
-          app.addMessage(app.container.results[key])
+          app.addMessage(app.container.results[key], false)
         }
       },
       error: function (data) {
@@ -64,13 +76,22 @@ var app = {
     $("#chats").children().remove();
   },
 
-  addMessage: function(message) {
+  addMessage: function(message, topOrBottom) {
     var textMessage = message.text;
     var userNameText = message.username;
     if (textMessage && userNameText) {
       if (textMessage.charAt(0).match(/^[0-9a-zA-Z]{1,16}$/)) {
         if (userNameText.charAt(0).match(/^[0-9a-zA-Z]{1,16}$/)) {
-          $('#chats').append("<div class='textbox'><a href=# id='user'>" + message.username + "</a>: " + message.text + "</div>");
+          // if (userNameText === '')
+          //   $('#chats').topOrBottom("<div class='textbox'><a href=# id='user'>" + message.username + "</a>: " + message.text + "</div>");
+
+          //$('#chats').topOrBottom("<div class='textbox'><a href=# id='user'>" + message.username + "</a>: " + message.text + "</div>");
+
+          if (topOrBottom) {
+            $('#chats').prepend("<div class='textbox'><a href=# id='user'>" + userNameText + "</a>: " + message.text + "</div>");
+          } else {
+            $('#chats').append("<div class='textbox'><a href=# id='user'>" + userNameText + "</a>: " + message.text + "</div>");
+          }     
         }
       }
     }
@@ -81,13 +102,14 @@ var app = {
   },
 
   addFriend: function(username) {
-    
+    console.log(username)
+    console.log("test")
   },
 
   handleSubmit: function(message) {
-
+    // sends message object to parse server
     app.send(message);
-    console.log("message is ", message)
-    app.addMessage(message);
+    // sends message object to addMessage method
+    app.addMessage(message, true);
   }
 }
